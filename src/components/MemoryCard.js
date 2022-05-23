144fc936c72418b06ed4236b892cd78e6881d5fd
Rafael Perger <rafael.perger@gmail.com>
2022-05-23 19:33:15 +0200

    Handle click on card

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,15 +1,22 @@
 import '../styles/MemoryCard.css';
-import { any, string } from 'prop-types';
+import { any, func, number, string } from 'prop-types';
 import React from 'react';
 
 const MemoryCard = (props) => {
   MemoryCard.propTypes = {
     flagSrc: any,
-    countryName: string
+    countryName: string,
+    id: number,
+    addCardToClickeCrads: func
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.addCardToClickeCrads(props.id);
   };
 
   return (
-    <div className="memory-card">
+    <div className="memory-card" id={props.countryName} onClick={handleClick}>
       <img className="flag" src={props.flagSrc} alt="flag" />
       <div className="country-name">{props.countryName}</div>
     </div>
